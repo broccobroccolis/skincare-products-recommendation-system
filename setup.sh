bd481ed3e1b39a5cbe3402411911ffc528c8f215
broccobroccolis <97378782+broccobroccolis@users.noreply.github.com>
2022-01-09 16:19:05 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,4 +10,4 @@
 #port = $PORTn
 #" > ~/.streamlit/config.toml
 
-mkdir -p ~/.streamlit/\necho "\n[general]n\nemail = "woeichyi_wan@outlook.com"n\n" > ~/.streamlit/credentials.toml\necho "\n[server]n\nheadless = truen\nenableCORS=falsen\nport = $PORTn\n" > ~/.streamlit/config.toml
+mkdir -p ~/.streamlit/\necho "\n[general]\nemail = "woeichyi_wan@outlook.com"\n" > ~/.streamlit/credentials.toml\necho "\n[server]\nheadless = true\nenableCORS=false\nport = $PORT\n" > ~/.streamlit/config.toml
