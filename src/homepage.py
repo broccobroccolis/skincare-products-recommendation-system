0ea90a71e40c70418bd85f5920196862986701a8
broccobroccolis <97378782+broccobroccolis@users.noreply.github.com>
2022-01-12 23:45:08 +0800

    Update homepage.py

--- src/homepage.py
+++ src/homepage.py
@@ -13,7 +13,7 @@ def app():
     st.header("""Skincare Products Recommendation Engine""")
     st.markdown("WIH3001 Data Science Project")
     st.markdown("Prepared by Wan Woei Chyi (17205866/1)")
-    image = Image.open('skincare.jpeg')
+    image = Image.open('img/skincare.jpeg')
     st.image(image)
     
     st.sidebar.caption("Navigate through the pages to explore more!")
