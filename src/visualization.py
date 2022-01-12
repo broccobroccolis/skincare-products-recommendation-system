955f20da0d8a25e42a4f04a9e4a45fcd86d9aa4e
broccobroccolis <97378782+broccobroccolis@users.noreply.github.com>
2022-01-12 23:40:36 +0800

    Update visualization.py

--- src/visualization.py
+++ src/visualization.py
@@ -9,14 +9,12 @@ Created on Tue Jan 11 16:20:02 2022
 import streamlit as st
 from PIL import Image
 import pandas as pd
-import matplotlib.pyplot as plt
-import altair as alt
 
 #from nltk.corpus import stopwords
 #from wordcloud import WordCloud, STOPWORDS
 
 def app():
-    cleaned_data = pd.read_csv("/Users/wanwoeichyi/Desktop/WIH3001 DS fyp/Cleaned_skindataall.csv")
+    cleaned_data = pd.read_csv("https://raw.githubusercontent.com/broccobroccolis/skinoclock/main/Cleaned_skindataall.csv")
     df = cleaned_data.copy()
     
     vis_type = st.multiselect("Select the type of visualizations you would like to explore: ", ("Users' Features Distribution","Ingredients Word Cloud"))
@@ -33,16 +31,16 @@ def featuresDistribution(df):
     for x in range(0, len(features)):
         if features[x] == "Skin Tone":
             st.caption("Skin Tone: ")
-            st.image(Image.open('/Users/wanwoeichyi/Desktop/WIH3001 DS fyp/img/skintone_dis.png'),width=450)
+            st.image(Image.open('skintone_dis.png'),width=450)
         elif features[x] == "Skin Type":
             st.caption("Skin Type: ")
-            st.image(Image.open('/Users/wanwoeichyi/Desktop/WIH3001 DS fyp/img/skintype_dis.png'),width=450)
+            st.image(Image.open('skintype_dis.png'),width=450)
         elif features[x] == "Eye Color":
             st.caption("Eye Color: ")
-            st.image(Image.open('/Users/wanwoeichyi/Desktop/WIH3001 DS fyp/img/eyecolor_dis.png'),width=450)
+            st.image(Image.open('eyecolor_dis.png'),width=450)
         elif features[x] == "Hair Color":
             st.caption("Hair Color: ")
-            st.image(Image.open('/Users/wanwoeichyi/Desktop/WIH3001 DS fyp/img/haircolor_dis.png'),width=450)
+            st.image(Image.open('haircolor_dis.png'),width=450)
             
         if x == len(features)-1:
                 st.success("Above is/are the users' distribution visualization(s) of your selected feature(s).")
@@ -61,19 +59,19 @@ def ingredientsWordCloud():
         
         if category[x] == 'Cleanser':
             st.caption("Cleanser:")
-            st.image(Image.open('/Users/wanwoeichyi/Desktop/WIH3001 DS fyp/img/cleanser_wordcloud.png'),width=450)
+            st.image(Image.open('cleanser_wordcloud.png'),width=450)
         elif category[x] == 'Toner':
             st.caption("Toner:")
-            st.image(Image.open('/Users/wanwoeichyi/Desktop/WIH3001 DS fyp/img/toner_wordcloud.png'),width=450)
+            st.image(Image.open('toner_wordcloud.png'),width=450)
         elif category[x] == 'Treatment':
             st.caption("Treatment:")
-            st.image(Image.open('/Users/wanwoeichyi/Desktop/WIH3001 DS fyp/img/treatment_wordcloud.png'),width=450)
+            st.image(Image.open('treatment_wordcloud.png'),width=450)
         elif category[x] == 'Moisturizer':
             st.caption("Moisturizer:")
-            st.image(Image.open('/Users/wanwoeichyi/Desktop/WIH3001 DS fyp/img/moisturizer_wordcloud.png'),width=450)
+            st.image(Image.open('moisturizer_wordcloud.png'),width=450)
         elif category[x] == 'Face Mask':
             st.caption("Face Mask:")
-            st.image(Image.open('/Users/wanwoeichyi/Desktop/WIH3001 DS fyp/img/facemask_wordcloud.png'),width=450)
+            st.image(Image.open('facemask_wordcloud.png'),width=450)
         
         if x == len(category)-1:
                 st.success("Above is/are the Word Cloud visualization(s) of the ingredients distribution of your selected product category(s).")
