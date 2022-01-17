ec0522cbbef5a7401e31954a07fe66792d602a58
broccobroccolis <97378782+broccobroccolis@users.noreply.github.com>
2022-01-17 16:37:38 +0800

    Update skinoclock.py

--- skinoclock.py
+++ skinoclock.py
@@ -11,19 +11,64 @@ import streamlit as st
 from src import homepage, about, rec, info, visualization
 
 from multipage import MultiPage
+import hydralit_components as hc
 
 # Create an instance of the app 
-app = MultiPage()
+#app = MultiPage()
 
-# Title of the main page
-st.title("Skin O'Clock")
+st.set_page_config(layout='wide',initial_sidebar_state='collapsed',)
 
 # Add all your applications (pages) here
-app.add_page("Homepage", homepage.app)
-app.add_page("Recommendations", rec.app)
-app.add_page("Visualizations", visualization.app)
-app.add_page("Extra Info", info.app)
-app.add_page("About", about.app)
+#app.add_page("Homepage", homepage.app)
+#app.add_page("Recommendations", rec.app)
+#app.add_page("Visualizations", visualization.app)
+#app.add_page("Extra Info", info.app)
+#app.add_page("About", about.app)
 
 # The main app
-app.run()
+#app.run()
+
+MENU = {
+    "Home" : homepage,
+    "Recommendations" : rec,
+    "Visualization" : visualization,
+    "Info" : info,
+    "About" : about,
+    
+}
+
+def main():
+
+    # specify the primary menu definition
+    menu_data = [
+        {'icon': "far fa-chart-bar", 'label':"Recommendations"},#no tooltip message
+        {'icon': "fas fa-desktop",'label':"Visualization"},
+        {'icon': "far fa-copy", 'label':"About"},
+        {'icon': "fas fa-info-circle", 'label':"Info"}, 
+    ]
+
+
+    over_theme = {'txc_inactive': '#FFFFFF','menu_background':'#DDB6B3'}
+    menu_id = hc.nav_bar(
+        menu_definition=menu_data,
+        override_theme=over_theme,
+        home_name='Home',
+        hide_streamlit_markers=False, #will show the st hamburger as well as the navbar now!
+        sticky_nav=True, #at the top or not
+        sticky_mode='pinned', #jumpy or not-jumpy, but sticky or pinned
+    )
+    
+    if menu_id == "Home":
+        homepage.app()
+    if menu_id == "Recommendations":
+        rec.app()
+    if menu_id == "Visualization":
+        visualization.app()
+    if menu_id == "Info":
+        info.app()
+    if menu_id == "About":
+        about.app()
+
+
+if __name__ == '__main__':
+    main()
